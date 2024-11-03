428ac36186fef5bb1d34b57e8521b130d3ced7ee
osamanisar8989 <osama.nisar@azm.dev>
2024-11-03 18:26:01 +0500

    Add translator

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,14 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 declare var $: any;
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
+
+  currentLang = localStorage.getItem('lang');
 
   ngAfterViewInit(): void {
     $('.slider-single').slick({
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, TranslateModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  translate: TranslateService = inject(TranslateService);
+  cdr = inject(ChangeDetectorRef)
+  currentLang = localStorage.getItem('lang') || this.translate.currentLang;
+
+  constructor() {
+    this.translate.use(this.currentLang);
+  }
+
+  translateText() {
+    const newLang = this.currentLang === 'ar' ? 'en' : 'ar';
+    this.translate.use(newLang);
+    this.currentLang = newLang;
+    localStorage.setItem('lang', newLang);
+    this.cdr.detectChanges();
+    window.location.reload();
+  }
 
 }
