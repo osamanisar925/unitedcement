f0d738ee4088b8e69102ed103b1fb047089074ba
osamanisar8989 <osama.nisar@azm.dev>
2024-11-05 01:07:35 +0500

    Add step 3 and step 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
 import { FooterComponent } from "./shared/footer/footer.component";
+import { TranslateService } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  translate: TranslateService = inject(TranslateService);
   title = 'unitedcement';
+  currentLang = localStorage.getItem('lang') || this.translate.currentLang;
 }
--- src/app/modules/shopping/shopping.component.ts
+++ src/app/modules/shopping/shopping.component.ts
@@ -2,18 +2,25 @@ import { Component } from '@angular/core';
 import { ShopStep1Component } from "./child-components/shop-step-1/shop-step-1.component";
 import { ShopStep2Component } from "./child-components/shop-step-2/shop-step-2.component";
 import { CommonModule } from '@angular/common';
+import { ShopStep3Component } from "./child-components/shop-step-3/shop-step-3.component";
+import { ShopStep4Component } from "./child-components/shop-step-4/shop-step-4.component";
 
 @Component({
   selector: 'app-shopping',
   standalone: true,
-  imports: [CommonModule, ShopStep1Component, ShopStep2Component],
+  imports: [CommonModule, ShopStep1Component, ShopStep2Component, ShopStep3Component, ShopStep4Component],
   templateUrl: './shopping.component.html',
   styleUrl: './shopping.component.scss'
 })
 export class ShoppingComponent {
-  step: string = '1';
+  step = localStorage.getItem('shop-step') || '1';
 
-  stepChanger(step: string) {
+  stepChanger(step: any) {
+    localStorage.setItem('shop-step', step);
     this.step = step;
+  }
+
+  ngOnDestroy() {
+    localStorage.removeItem('shop-step');
   }
 }
